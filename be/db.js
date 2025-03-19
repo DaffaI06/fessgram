32fd0f44ba88393b51e3220c3d7e1545463ed053
DaffaI06 <daffa2.ismail178@gmail.com>
2025-03-19 23:38:54 +0700

    using env for db.js

--- be/db.js
+++ be/db.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const {Pool} = require('pg');
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    database: 'fessdb',
-    password: 'myBackend',
-    port: 5432
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT
 });
 
 module.exports = pool;
